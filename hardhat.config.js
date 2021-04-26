355f4678ed2c6778f88bab86ef003ff313c4a230
janruls1 <janruls1@gmail.com>
2021-04-26 17:24:33 +0530

    added frx farm and frx vault

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,6 +2,7 @@ require("@nomiclabs/hardhat-waffle");
 require("hardhat-deploy");
 require("dotenv").config();
 require("hardhat-gas-reporter");
+require("@nomiclabs/hardhat-etherscan");
 
 const privateKeys = process.env.PRIVATE_KEYS || ""
 
@@ -18,6 +19,11 @@ module.exports = {
       },
     }
   },
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: process.env.ETHERSCAN_API_KEY
+  },
   gasReporter: {
     currency: 'USD',
     gasPrice: 119,
@@ -31,7 +37,7 @@ module.exports = {
       timeout: 1800000
     },
     ropsten: {
-      'url': `https://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      url: `https://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}`,
       accounts: privateKeys.split(','),
       gas: 7000000,
       gasPrice: 25000000000,
@@ -39,6 +45,16 @@ module.exports = {
       networkCheckTimeout: 20000,
       skipDryRun: true,
       chainId: 3
-    }
+    },
+    kovan: {
+      url: `https://kovan.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: privateKeys.split(','),
+      gas: 7000000,
+      gasPrice: 25000000000,
+      network_id: 42,
+      networkCheckTimeout: 20000,
+      skipDryRun: true,
+      chainId: 42
+    },
   }
 };
